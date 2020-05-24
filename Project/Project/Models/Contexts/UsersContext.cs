41babb6c243891584fc079a7065ca4c311497ab2
Dikosha <48882652+Dikosha@users.noreply.github.com>
2020-05-24 14:58:52 +0600

    add roles

--- Project/Project/Models/Contexts/UsersContext.cs
+++ Project/Project/Models/Contexts/UsersContext.cs
@@ -10,7 +10,7 @@ namespace Project.Models.Contexts
     public class UserContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-
+        public DbSet<Role> Roles { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Category> Categories { get; set; }
--- Project/Project/Startup.cs
+++ Project/Project/Startup.cs
@@ -34,6 +34,7 @@ namespace Project
                 .AddCookie(options => //CookieAuthenticationOptions
                 {
                     options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+                    options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
                 });
             services.AddControllersWithViews();
         }
